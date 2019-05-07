3e6f05257b92b100a4dbb9c8e42bc5ae9e603c95
Lucas Matos <matoslucas@hotmail.com>
2019-05-07 10:58:58 -0600

    fix NaN bug

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,96 +5,102 @@ import moment from 'moment'
  * @param {object} date 
  * @param {Array} data 
  */
+const COMPLEXITY = 'Complexity (# of Plexes)'
+
+const checkifIsNan = (value) => {
+    return value ? Number(value) : 0
+}
+
 export const getTotalByWeekofTaskTypeWraper = (field, date, data) => {
     let total = 0
     let complexity = 0
-    let setScaff = {total: 0, complexity: 0}
-    let paperWire = {total: 0, complexity: 0}
-    let housewrap = {total: 0, complexity: 0}
-    let lathInspection = {total: 0, complexity: 0}
-    let weatherBarrierInspection = {total: 0, complexity: 0}
-    let tentHeat = {total: 0, complexity: 0}
-    let stuccoBrown = {total: 0, complexity: 0}
-    let watering = {total: 0, complexity: 0}
-    let jChannel = {total: 0, complexity: 0}
-    let siding = {total: 0, complexity: 0}
-    let specialCorbels = {total: 0, complexity: 0}
-    let paint = {total: 0, complexity: 0}
-    let rock = {total: 0, complexity: 0}
-    let brick = {total: 0, complexity: 0}
-    let soffitFascia = {total: 0, complexity: 0}
-    let stuccoColor = {total: 0, complexity: 0}
-    let shutters = {total: 0, complexity: 0}
-    let miscWork = {total: 0, complexity: 0}
-    let cleanup = {total: 0, complexity: 0}
-    let gutters = {total: 0, complexity: 0}
-    let vpoWork = {total: 0, complexity: 0}
-    let warrantyWork = {total: 0, complexity: 0}
-    let touchUps = {total: 0, complexity: 0}
-    let removeScaff = {total: 0, complexity: 0}
-    let foundationPlaster = {total: 0, complexity: 0}
-    let finalInspection = {total: 0, complexity: 0}
+    let setScaff = { total: 0, complexity: 0 }
+    let paperWire = { total: 0, complexity: 0 }
+    let housewrap = { total: 0, complexity: 0 }
+    let lathInspection = { total: 0, complexity: 0 }
+    let weatherBarrierInspection = { total: 0, complexity: 0 }
+    let tentHeat = { total: 0, complexity: 0 }
+    let stuccoBrown = { total: 0, complexity: 0 }
+    let watering = { total: 0, complexity: 0 }
+    let jChannel = { total: 0, complexity: 0 }
+    let siding = { total: 0, complexity: 0 }
+    let specialCorbels = { total: 0, complexity: 0 }
+    let paint = { total: 0, complexity: 0 }
+    let rock = { total: 0, complexity: 0 }
+    let brick = { total: 0, complexity: 0 }
+    let soffitFascia = { total: 0, complexity: 0 }
+    let stuccoColor = { total: 0, complexity: 0 }
+    let shutters = { total: 0, complexity: 0 }
+    let miscWork = { total: 0, complexity: 0 }
+    let cleanup = { total: 0, complexity: 0 }
+    let gutters = { total: 0, complexity: 0 }
+    let vpoWork = { total: 0, complexity: 0 }
+    let warrantyWork = { total: 0, complexity: 0 }
+    let touchUps = { total: 0, complexity: 0 }
+    let removeScaff = { total: 0, complexity: 0 }
+    let foundationPlaster = { total: 0, complexity: 0 }
+    let finalInspection = { total: 0, complexity: 0 }
 
     data.filter(item => {
         return moment(item[field]).isBetween(date.start, date.end);
     }).forEach(item => {
         // console.log(item['Task Type'])
         total++
-        complexity += Number(item['Complexity (# of Plexes)'])
-            
+        complexity += checkifIsNan(item[COMPLEXITY])
+
 
         switch (item['Task Type']) {
-            case 'Set Scaff': setScaff.total++; setScaff.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Set Scaff': setScaff.total++; setScaff.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Paper/Wire': paperWire.total++; paperWire.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Paper/Wire': paperWire.total++; paperWire.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Housewrap': housewrap.total++; housewrap.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Housewrap': housewrap.total++; housewrap.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Lath Inspection': lathInspection.total++; lathInspection.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Lath Inspection': lathInspection.total++; lathInspection.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Weather Barrier Inspection': weatherBarrierInspection.total++; weatherBarrierInspection.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Weather Barrier Inspection': weatherBarrierInspection.total++; weatherBarrierInspection.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Tent & Heat': tentHeat.total++; tentHeat.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Tent & Heat': tentHeat.total++; tentHeat.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Stucco Brown': stuccoBrown.total++; stuccoBrown.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Stucco Brown': stuccoBrown.total++; stuccoBrown.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Watering': watering.total++; watering.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Watering': watering.total++; watering.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'J-Channel': jChannel.total++; jChannel.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'J-Channel': jChannel.total++; jChannel.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Siding': siding.total++; siding.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Siding': siding.total++; siding.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Special Corbels': specialCorbels.total++; specialCorbels.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Special Corbels': specialCorbels.total++; specialCorbels.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Paint': paint.total++; paint.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Paint': paint.total++; paint.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Rock': rock.total++; rock.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Rock': rock.total++; rock.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Brick': brick.total++; brick.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Brick': brick.total++; brick.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Soffit/Fascia': soffitFascia.total++; soffitFascia.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Soffit/Fascia': soffitFascia.total++; soffitFascia.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Stucco Color': stuccoColor.total++; stuccoColor.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Stucco Color': stuccoColor.total++; stuccoColor.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Shutters': shutters.total++; shutters.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Shutters': shutters.total++; shutters.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Misc. Work': miscWork.total++; miscWork.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Misc. Work': miscWork.total++; miscWork.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Cleanup': cleanup.total++; cleanup.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Cleanup': cleanup.total++; cleanup.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Gutters': gutters.total++; gutters.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Gutters': gutters.total++; gutters.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'VPO Work': vpoWork.total++; vpoWork.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'VPO Work': vpoWork.total++; vpoWork.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Warranty Work': warrantyWork.total++; warrantyWork.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Warranty Work': warrantyWork.total++; warrantyWork.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Touch Ups': touchUps.total++; touchUps.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Touch Ups': touchUps.total++; touchUps.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Remove Scaff': removeScaff.total++; removeScaff.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Remove Scaff': removeScaff.total++; removeScaff.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Foundation Plaster': foundationPlaster.total++; foundationPlaster.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Foundation Plaster': foundationPlaster.total++; foundationPlaster.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
-            case 'Final Inspection': finalInspection.total++; finalInspection.complexity += Number(item['Complexity (# of Plexes)']);
+            case 'Final Inspection': finalInspection.total++; finalInspection.complexity += checkifIsNan(item[COMPLEXITY]);
                 break;
             default:
         }
